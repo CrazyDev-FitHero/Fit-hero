e5b8aedf3f7a494df0565f9201537f504086dea8
Samy ZIANE <samy.ziane-pro@outlook.com>
2024-09-12 09:51:50 +0200

    update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>FitHero - @yield('title')</title>
-
+        <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../css/custom.css">
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 {{--        <link rel="stylesheet" href="../css/app.css">--}}
